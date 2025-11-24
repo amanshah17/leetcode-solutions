2dff4c11bc2c20a5ea9d349612aa79a1bb88cdf0
Aman Kumar Shah <shahamankumar054@gmail.com>
2025-11-24 20:38:28 +0530

    Time: 101 ms (37.73%) | Memory: 51 MB (27.16%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -7,7 +7,7 @@ public class Solution {
 
         isPrime[0] = false;
         isPrime[1] = false;
-        for (int i = 2; i * i < n; i++) {
+        for (int i = 2;i <= (int)Math.sqrt(n); i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j < n; j += i) {
                     isPrime[j] = false;
