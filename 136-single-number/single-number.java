2b707c348248c68a79eef2e5e12e7ce3e3e3eca1
Aman Kumar Shah <shahamankumar054@gmail.com>
2025-12-02 22:41:12 +0530

    Time: 2 ms (30.24%) | Memory: 48.1 MB (5.28%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,9 +1,30 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-        for(int a:nums){
-            result ^= a;
+        // int result = 0;
+        // for(int a:nums){
+        //     result ^= a;
+        // }
+        // return result;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int x : nums){
+            if(x < min){
+                min = x;
+            }
+            if(x > max){
+                max = x;
+            }
         }
-        return result;
+        int[] freq = new int[max-min+1];
+        for(int x: nums){
+            freq[x-min]++;
+        }
+
+        for(int i = 0; i < freq.length; i++){
+            if(freq[i] == 1){
+                return i+min;
+            }
+        }
+        return -1;
     }
 }
