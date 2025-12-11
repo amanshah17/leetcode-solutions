feeb9e0fd48f9d74e2a8c4bcb17a137039dcc921
Aman Kumar Shah <shahamankumar054@gmail.com>
2025-12-11 23:34:54 +0530

    Time: 3 ms (90.15%) | Memory: 44.2 MB (76.89%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        Map<Character,Integer> map1 = new HashMap<>();
-        Map<Character,Integer> map2 = new HashMap<>();
-        for(Character c : s.toCharArray()){
-            map1.put(c,map1.getOrDefault(c,0)+1);
-        }
-        for(Character c : t.toCharArray()){
-            map2.put(c,map2.getOrDefault(c,0)+1);
-        }
-        for(int i = 0; i < s.length(); i++){
-            Character c = s.charAt(i);
-            if(!map2.containsKey(c) || ! map1.get(c).equals(map2.get(c))){
-                return false;
-            }
-        }
-    return true;
+        int[] freq1 = frequency(s);
+    	int[] freq2 = frequency(t);
+    	for(int i = 0; i < 26; i++) {
+    		if(freq1[i] != freq2[i]) {
+    			return false;
+    		}
+    	}
+    	return true;
     }
+    private static int[] frequency(String str) {
+    	 int[] freq = new int[26];
+    	 for(int i = 0; i < str.length(); i++) {
+    		 freq[str.charAt(i)-97]++;
+    	 }
+    	 return freq;
+     }
 }
